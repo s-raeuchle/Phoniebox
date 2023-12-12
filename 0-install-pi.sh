94212c90cd148b8611ce973a9c0ee3602e0cfa22
sraeuchle <s.raeuchle@gmx.de>
2023-12-12 22:23:51 +0100

    feat: onofshim install

--- 0-install-pi.sh
+++ 0-install-pi.sh
@@ -7,4 +7,6 @@ cat ./pi/config.txt | ssh pi@pi.local "sudo tee /boot/config.txt"
 echo "ALSA Hifiberry setup"
 cat ./pi/asound.conf | ssh pi@pi.local "sudo tee /etc/asound.conf"	
 
+ssh pi@pi.local "curl https://get.pimoroni.com/onoffshim | bash"
+
 ssh pi@pi.local "sudo reboot"
