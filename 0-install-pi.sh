d64e74419a8e446d6b9a1cf05d3cd449ee239b21
sraeuchle <s.raeuchle@gmx.de>
2023-12-12 22:25:09 +0100

    feat: onoffshim config

--- 0-install-pi.sh
+++ 0-install-pi.sh
@@ -7,6 +7,8 @@ cat ./pi/config.txt | ssh pi@pi.local "sudo tee /boot/config.txt"
 echo "ALSA Hifiberry setup"
 cat ./pi/asound.conf | ssh pi@pi.local "sudo tee /etc/asound.conf"	
 
+echo "OnOf Shim setup"
 ssh pi@pi.local "curl https://get.pimoroni.com/onoffshim | bash"
+cat ./pi/cleanshutd.conf | ssh pi@pi.local "sudo tee /etc/cleanshutd.conf"	
 
 ssh pi@pi.local "sudo reboot"
